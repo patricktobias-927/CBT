c677e0cf29a6493c213183d3e99eb9f20f03d8b9
PatrickTobias <patricktobias.business@gmail.com>
2021-03-09 11:48:00 +0800

    Update section_template.xls per school

--- application/controllers/Export_add_section_perschool.php
+++ application/controllers/Export_add_section_perschool.php
@@ -18,7 +18,7 @@ class Export_add_section_perschool extends CI_Controller {
 
   $object->setActiveSheetIndex(0);
 
-  $table_columns = array("School Name", "School Code", "Grade", "Section Name", "Batch", "Section Code", "School Year", "Population");
+  $table_columns = array("school_code", "grade", "section", "section_code", "sy");
 
   $column = 0;
 
@@ -34,15 +34,12 @@ class Export_add_section_perschool extends CI_Controller {
 
   foreach($section_data as $row)
   {
-   $object->getActiveSheet()->setCellValueByColumnAndRow(0, $excel_row, $row->school_name);
-   $object->getActiveSheet()->setCellValueByColumnAndRow(1, $excel_row, $row->school_code);
-   $object->getActiveSheet()->setCellValueByColumnAndRow(2, $excel_row, $row->grade);
-   $object->getActiveSheet()->setCellValueByColumnAndRow(3, $excel_row, $row->section_name);
-   $object->getActiveSheet()->setCellValueByColumnAndRow(4, $excel_row, $row->batch);
-   $object->getActiveSheet()->setCellValueByColumnAndRow(5, $excel_row, $row->section_code);
-   $object->getActiveSheet()->setCellValueByColumnAndRow(6, $excel_row, $row->school_year);
-   $object->getActiveSheet()->setCellValueByColumnAndRow(7, $excel_row, $row->population);
-   $excel_row++;
+    $object->getActiveSheet()->setCellValueByColumnAndRow(1, $excel_row, $row->school_code);
+    $object->getActiveSheet()->setCellValueByColumnAndRow(2, $excel_row, $row->grade);
+    $object->getActiveSheet()->setCellValueByColumnAndRow(3, $excel_row, $row->section_name);
+    $object->getActiveSheet()->setCellValueByColumnAndRow(5, $excel_row, $row->section_code);
+    $object->getActiveSheet()->setCellValueByColumnAndRow(6, $excel_row, $row->school_year);
+    $excel_row++;
   }
 
   $object_writer = PHPExcel_IOFactory::createWriter($object, 'Excel5');
